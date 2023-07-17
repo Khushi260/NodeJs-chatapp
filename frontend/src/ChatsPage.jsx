4dfd47810bb33fc9e9fb3aeff4b65168e5ae258f
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2023-07-17 11:22:37 +0530

    Add files via upload

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <PrettyChatWindow
+                projectId='56e35c4b-270d-488f-897f-5004057a8260'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100vh' }}
+
+            />
+
+        </div>
+    )
+}
+
+export default ChatsPage
